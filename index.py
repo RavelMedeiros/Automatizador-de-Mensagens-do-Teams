1b7a1a0272013ac9f1c7524c8026d2da65f033b1
Ravel <112495220+RavelMedeiros@users.noreply.github.com>
2024-01-01 22:06:09 -0400

    refatoracao

--- index.py
+++ index.py
@@ -1,64 +1,58 @@
-import pyautogui
-import time
-import pyperclip
-import pytesseract
-from pytesseract import Output
 import json
 import locale
+import pyautogui
+import pyperclip
+import pytesseract
+import time
 from datetime import datetime
+from pytesseract import Output
 
-locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
+# Configurações do Tesseract e Localização
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
 
-contatos_encontrados = []
-contatos_nao_encontrados = []
+# Coordenadas e dimensões da captura de tela
+COORDENADAS_CAPTURA = (760, 100, 527, 350)
+AJUSTE_Y = 90
+AJUSTE_ALTURA = 1
 
-def carregar_debitos(caminho_arquivo):
+# Coordenadas de interação no Teams
+COORDENADA_CLIQUE_NOME = (910, 123)
+COORDENADA_CLIQUE_MENSAGEM = (957, 232)
+COORDENADA_ESCRITA_MENSAGEM = (1147, 969)
+
+def carregar_json(caminho_arquivo):
+    """Carrega um arquivo JSON e retorna seu conteúdo."""
     with open(caminho_arquivo, 'r') as file:
         return json.load(file)
 
 def verificar_contato_na_lista(nome_formatado):
+    """Verifica se um contato está na lista do Teams."""
     try:
         time.sleep(2)
-        # Coordenadas originais
-        x, y, largura, altura = 760, 100, 527, 350
-
-        # Para descer mais a captura, aumente o valor de 'y'
-        y_novo = y + 90  # Isso descerá a captura em 50 pixels
-
-        # Você também pode querer ajustar a altura se precisar de uma região maior ou menor
-        altura_nova = altura + 1  # Isso aumentará a altura da captura em 50 pixels
-
-        # Captura com as novas coordenadas
-        captura = pyautogui.screenshot(region=(x, y_novo, largura, altura_nova))
-
+        x, y, largura, altura = COORDENADAS_CAPTURA
+        captura = pyautogui.screenshot(region=(x, y + AJUSTE_Y, largura, altura + AJUSTE_ALTURA))
         texto = pytesseract.image_to_string(captura, lang='por')
-
         return nome_formatado in texto
     except Exception as e:
         print(f"Erro ao verificar o contato {nome_formatado}: {e}")
         return False
 
-def carregar_mensagem(caminho_arquivo):
-    with open(caminho_arquivo, 'r', encoding='utf-8') as file:
-        return file.read()
-
-def enviar_mensagem_no_teams(nome, nome_formatado, debito):
-    global contatos_encontrados, contatos_nao_encontrados
+def enviar_mensagem_no_teams(nome, nome_formatado, debito, mensagem_template):
+    """Envia uma mensagem no Microsoft Teams."""
     try:
         time.sleep(5)
         mes_atual = "dezembro"
-        template_mensagem = carregar_mensagem('mensagem.txt')
-        mensagem = template_mensagem.format(nome=nome, mes_atual=mes_atual, debito=debito)
+        mensagem = mensagem_template.format(nome=nome, mes_atual=mes_atual, debito=debito)
 
-        pyautogui.click(x=910, y=123)
+        pyautogui.click(*COORDENADA_CLIQUE_NOME)
         pyautogui.write(nome_formatado)
 
         if verificar_contato_na_lista(nome_formatado):
-            pyautogui.click(x=957, y=232)
+            pyautogui.click(*COORDENADA_CLIQUE_MENSAGEM)
             time.sleep(1)
 
-            pyautogui.click(x=1147, y=969)
+            pyautogui.click(*COORDENADA_ESCRITA_MENSAGEM)
             time.sleep(1)
 
             pyautogui.press('delete')
@@ -66,38 +60,40 @@ def enviar_mensagem_no_teams(nome, nome_formatado, debito):
             pyautogui.hotkey('ctrl', 'v')
             pyautogui.press('enter')
 
-            pyautogui.click(x=910, y=123)
+            pyautogui.click(*COORDENADA_CLIQUE_NOME)
             pyautogui.hotkey('ctrl', 'a')
             pyautogui.press('delete')
 
             print(f"Mensagem enviada com sucesso para {nome}.")
-            contatos_encontrados.append(nome_formatado)
+            return True
         else:
             print(f"Contato {nome_formatado} não encontrado na lista.")
-            pyautogui.click(x=910, y=123)
+            pyautogui.click(*COORDENADA_CLIQUE_NOME)
             pyautogui.hotkey('ctrl', 'a')
             pyautogui.press('delete')
-            contatos_nao_encontrados.append(nome_formatado)
+            return False
 
     except Exception as e:
         print(f"Erro ao enviar mensagem para {nome_formatado}: {e}")
+        return False
 
 def main():
-    caminho_arquivo = 'D:\\PROJETOS\\AutomatizacaoBebidas\\debitos.json'
-    nomes_e_debitos = carregar_debitos(caminho_arquivo)
+    caminho_arquivo_debitos = 'D:\\PROJETOS\\AutomatizacaoBebidas\\debitos.json'
+    caminho_arquivo_mensagem = 'mensagem.txt'
+    nomes_e_debitos = carregar_json(caminho_arquivo_debitos)
+    mensagem_template = carregar_json(caminho_arquivo_mensagem)
+
+    contatos_encontrados = []
+    contatos_nao_encontrados = []
 
     for item in nomes_e_debitos:
-        enviar_mensagem_no_teams(item['nome'], item['nome_formatado'], item['debito'])
+        sucesso = enviar_mensagem_no_teams(item['nome'], item['nome_formatado'], item['debito'], mensagem_template)
+        if sucesso:
+            contatos_encontrados.append(item['nome_formatado'])
+        else:
+            contatos_nao_encontrados.append(item['nome_formatado'])
 
-    print("\nRelatório de Envio de Mensagens:")
-    print("Contatos Encontrados e Mensagens Enviadas:")
-    for contato in contatos_encontrados:
-        print(contato)
-
-    print("\nContatos Não Encontrados:")
-    for contato in contatos_nao_encontrados:
-        print(contato)
-
+    # Geração de relatório
     with open('relatorio_contatos.txt', 'w') as file:
         file.write("Contatos Encontrados e Mensagens Enviadas:\n")
         file.write('\n'.join(contatos_encontrados))
